b49f1f060f258ded58e71cbf81dbc0084c5b7c91
matias-aburto <matiasaburto653@gmail.com>
2023-10-08 15:21:11 -0300

    primera version estable

---
+++ rut-formatter.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var rutInput = document.getElementById("billing_rut");
+  
+    rutInput.addEventListener("input", function() {
+      var rut = this.value;
+      var formattedRut = formatRut(rut);
+      this.value = formattedRut;
+    });
+  
+    function formatRut(rut) {
+      // Elimina cualquier caracter no numérico y las letras Kk al final
+      rut = rut.replace(/[^\dKk]/g, "");
+      
+      // Asegura que el RUT tenga una longitud máxima de 9 dígitos
+      rut = rut.substr(0, 9);
+      
+      // Divide el RUT en parte antes del guión y el dígito verificador
+      var rutBody = rut.substring(0, rut.length - 1);
+      var rutVerifier = rut.charAt(rut.length - 1);
+  
+      // Verifica que haya al menos 7 caracteres antes de agregar el guión
+      if (rutBody.length >= 7) {
+        // Formatea la parte antes del guión con puntos y agrega el guión y el dígito verificador
+        var formattedRut = rutBody.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.") + "-" + rutVerifier;
+      } else {
+        // Si no hay al menos 7 caracteres, no se agrega el guión
+        var formattedRut = rutBody + rutVerifier;
+      }
+      
+      return formattedRut;
+    }
+  });
---
+++ rut-formatter.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Plugin Name: Formateador de RUT
+Description: Formatea y valida el RUT chileno en formularios.
+Version: 1.1
+Author: <a href="https://simetry.cl" target="_blank">Simetry Code</a>
+*/
+// Agregar enlace a la página de ajustes del plugin
+function rut_formatter_add_settings_link($links) {
+    $settings_link = '<a href="admin.php?page=rut-formatter-settings">Ajustes</a>';
+    $changelog_link = '<a href="' . plugin_dir_url(__FILE__) . 'changelog.txt">Changelog</a>';
+    array_unshift($links, $settings_link);
+    array_unshift($links, $changelog_link);
+    return $links;
+}
+
+$plugin_basename = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin_basename", 'rut_formatter_add_settings_link');
+
+// Agregar scripts y estilos
+function rut_formatter_enqueue_scripts() {
+    wp_enqueue_script('rut-formatter', plugin_dir_url(__FILE__) . 'rut-formatter.js', array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'rut_formatter_enqueue_scripts');
+
+// Validación del RUT en el lado del servidor
+function rut_formatter_validate_rut() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['woocommerce_checkout_place_order'])) {
+        $billing_rut = sanitize_text_field($_POST['billing_rut']);
+        
+        if (!rut_formatter_validate_rut_format($billing_rut)) {
+            // RUT no válido, muestra un mensaje de error o realiza la acción correspondiente.
+            wc_add_notice("El RUT ingresado no es válido.", 'error');
+        }
+    }
+}
+
+function rut_formatter_validate_rut_format($rut) {
+    // Implementa aquí la lógica de validación del RUT en PHP.
+    // Retorna true si es válido, false en caso contrario.
+    
+    // Ejemplo de validación básica del RUT (puedes personalizar esto)
+    $rut = preg_replace('/[^0-9Kk]/', '', $rut);
+    $rut = str_replace('K', 'k', $rut);
+    
+    if (strlen($rut) !== 9) {
+        return false;
+    }
+    
+    $rutDigits = substr($rut, 0, 8);
+    $rutVerifier = substr($rut, 8, 1);
+    
+    $rutVerifierCalculated = rut_formatter_calculate_verifier_digit($rutDigits);
+    
+    return $rutVerifier === $rutVerifierCalculated;
+}
+
+function rut_formatter_calculate_verifier_digit($rutDigits) {
+    $rutDigits = strrev($rutDigits);
+    $factor = 2;
+    $sum = 0;
+    
+    for ($i = 0; $i < strlen($rutDigits); $i++) {
+        $sum += intval($rutDigits[$i]) * $factor;
+        $factor = ($factor % 7) + 1;
+    }
+    
+    $remainder = $sum % 11;
+    $verifier = 11 - $remainder;
+    
+    return ($verifier == 10) ? 'k' : strval($verifier);
+}
+
+add_action('woocommerce_review_order_before_payment', 'rut_formatter_validate_rut');
+?>
